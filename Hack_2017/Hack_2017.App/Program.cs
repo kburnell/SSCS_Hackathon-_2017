b5e6cff5b5382cd72f1a91aaf41d07949ee55b75
Keith Burnell <keith@kburnell.com>
2017-12-05 16:46:27 -0600

    initial sln creation

---
+++ Hack_2017/Hack_2017.App/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Hack_2017.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
