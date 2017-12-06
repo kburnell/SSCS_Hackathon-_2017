9ba234ee6ecc6351e9e971f78c3d0554a7b53bad
kburnell <keith@kburnell.com>
2017-12-06 04:55:24 -0600

    new sln

---
+++ Hack2017/Hack2017/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Hack2017
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+        }
+    }
+}
