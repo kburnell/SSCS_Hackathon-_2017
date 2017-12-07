7ee293805ed2f09d63436871e59cde7c093c831a
Sarah <sarahbourt@gmail.com>
2017-12-07 09:58:23 -0800

    added the remainder of linear regression function

--- Hack2017/Hack2017/Math/Statistics.cs
+++ Hack2017/Hack2017/Math/Statistics.cs
@@ -21,7 +21,7 @@ namespace Hack2017.Math
             return collection.Sum(selector);
         }
 
-        public static decimal Sigma(IEnumerable<decimal> values)
+        public static decimal Sigma(this IEnumerable<decimal> values)
         {
             return values.Sum();
         }
@@ -39,21 +39,41 @@ namespace Hack2017.Math
             return y;
         }
 
-        public static IEnumerable<PlotData> LinearRegression<T>(this IEnumerable<T> series, Func<T, decimal> xSelector, Func<T, decimal> YSelector)
+        public static Func<T, decimal> LinearRegression<T>(this IEnumerable<T> series, Func<T, decimal> xSelector, Func<T, decimal> ySelector)
         {
             var n = series.Count();
+
+            //get our sigmas
             var sigmaX = series.Sigma(xSelector);
-            throw new NotImplementedException();
+            var sigmaY = series.Sigma(ySelector);
+            var sigmaXY = series.Sigma(item => xSelector(item) * ySelector(item));
+            var sigmaX2 = series.Sigma(item => (decimal)System.Math.Pow((double)xSelector(item), 2));
+            var sigmaY2 = series.Sigma(item => (decimal)System.Math.Pow((double)ySelector(item), 2));
+
+            //get opur A & B values
+            var a = LinearRegressionPointA(n, sigmaX, sigmaY, sigmaXY, sigmaX2, sigmaY2);
+            var b = LinearRegressionPointB(n, sigmaX, sigmaY, sigmaXY, sigmaX2, sigmaY2);
+
+            //calculate our Y values off each item's X
+            var plotItems = new List<PlotData>();
+
+            return x => Slope(xSelector(x), a, b);
         }
 
         public static decimal LinearRegressionPointA(int n, decimal sigmaX, decimal sigmaY, decimal sigmaXY, decimal sigmaXsqr, decimal sigmaYsqr)
         {
-            throw new NotImplementedException();
+            var top = sigmaY * sigmaXsqr - sigmaX * sigmaXY;
+            var bottom = n * sigmaXsqr - sigmaXsqr;
+
+            return top / bottom;
         }
 
         public static decimal LinearRegressionPointB(int n, decimal sigmaX, decimal sigmaY, decimal sigmaXY, decimal sigmaXsqr, decimal sigmaYsqr)
         {
-            throw new NotImplementedException();
+            var top = n * sigmaXY - sigmaX * sigmaY;
+            var bottom = n * sigmaXsqr - sigmaXsqr;
+
+            return top / bottom;
         }
     }
 
